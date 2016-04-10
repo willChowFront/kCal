d41d959b1d71707110494a5f53947484d6794122
赵凯康 <zhaokaikangwill@foxmail.com>
2016-04-10 21:24:05 +0800

    kCal.js

--- kCal.js
+++ kCal.js
@@ -11,27 +11,94 @@ var kCal = function(config){
 	var
 		monDay = [31,28,31,30,31,30,31,31,30,31,30,31],
 		leapMonDay = [31,29,31,30,31,30,31,31,30,31,30,31],
+		calId = config.calId ？ config.calId : {console.log('Error:未指定万年历的父元素id';return;)}
 		currentDate = config.currentDate ? config.currentDate : new Date().getDate(),
 		currentMonth = config.currentMonth ? config.currentMonth : new Date().getMonth(),
 		currentYear = config.currentYear ? config.currentYear : new Date().getYear(); 
 	
 	/**
 	 * [createStaticCal 生成万年历静态部分html]
+	 * @param  {[string]} calId        [万年历的父元素id]
 	 * @param  {[number]} currentYear  [指定年份]
 	 * @param  {[number]} currentMonth [指定月份]
 	 * @return {[type]}              [description]
+	 *
+	 * 
 	 */
-	function createStaticCal(currentYear,currentMonth){
+	function createStaticCal(calId,currentYear,currentMonth){
+		var
+			wrapEle = document.getElementById(calId),
+			headDiv = document.createElement('div');
+		wrapEle.id = 'k_CalWrap';//方便为万年历添加样式。
+
 
 	}
 	/**
 	 * [repeatCreateNode 重复生成多个节点]
-	 * @param  {[number]} nums [需要生成的节点子数量]
-	 * @param  {[string]} childNode [需要生成的子节点]
-	 * @return {[string]} nodeList [生成的子节点列表]
+	 * @param  {[node]} parentNode [添加子节点的父节点]
+	 * @param  {[json]}   data       [如下所示,属性名为要添加的元素名，noneChild:表示有无子节点,textNode表示文本节点,当文本节点为数组时表示有多个元素需要生成，attribute表示该元素的属性
+	 *                               	{
+	 *										"option":{
+	 *											"noneChild":true,
+	 *											 textNode:[1900,...2010]
+	 *										}
+	 *									}
+	 *									{
+	 *										"span":{
+	 *											"noneChild":false,
+	 *											"childNode";{
+	 *												a":{
+	 *												   "noChild": true,
+	 *												   "textNode":"左减",
+	 *												   "attribute":{
+	 *													 "href":"javascript:;"
+	 *												   }
+	 *											  	}
+	 *											}
+	 *										}
+	 *									}
+	 *									{
+	 *										"div":{
+	 *											"a":{
+	 *												"noneChild": true,
+	 *												"textNode":"左减",
+	 *												"attribute":{
+	 *													"href":"javascript:;"
+	 *												}
+	 *											}
+	 *										}
+	 *									}
+	 *                               ]
+	 * @return {[type]}              []
 	 */
-	function repeatCreateNode(nums , childNode){
-		for()
+	function repeatCreateNode(parentNode , data){
+		for(var eleName in data){
+			// if(typeof data.eleName === Array){
+			// 	data.eleName.forEach(function(item){
+			// 		var
+			// 			ele = document.createElement('"'+eleName+'"'),
+			// 			textNode = document.createTextNode('"'+item+'"');
+			// 		ele.appendChild(textNode);
+			// 		parentNode.appendChild(ele); 
+			// 	})
+			// }
+			// else if(typeof data.eleName === Object){
+			// 	var
+			// 		parentEle = document.createElement('"'+eleName+'"'),
+			// 	repeatCreateNode(parentEle , data.eleName); 
+			// }
+			// else if(typeof data.eleName === Number || typeof data.eleName === String){
+			// 	var
+			// 		childEle = document.createElement('"'+eleName+'"'),
+			// 		textNode = document.createTextNode('"'+data.eleName+'"');
+			// 	childEle.appendChild(textNode);
+			// }
+			if(data.eleName.noneChild){
+				var
+					ele = document.createElement('"'+eleName+'"'),
+
+			}
+		}
 	}
 	
 	/**
